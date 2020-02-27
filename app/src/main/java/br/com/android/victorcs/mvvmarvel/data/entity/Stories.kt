8005fdf2dee2f0c18cbdbd9d4bf3d2c2a246a830
victorcs <victo.santiago@grupopan.com>
2020-02-27 15:12:17 -0300

    refactor: Change path data files.

--- app/src/main/java/br/com/android/victorcs/mvvmarvel/data/entity/Stories.kt
+++
@@ -1,8 +0,0 @@
-package br.com.android.victorcs.mvvmarvel.data.entity
-
-data class Stories(
-    val available: Int,
-    val collectionURI: String,
-    val items: List<Any>,
-    val returned: Int
-)
