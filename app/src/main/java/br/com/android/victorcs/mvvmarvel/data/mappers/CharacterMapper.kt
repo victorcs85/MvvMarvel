26dd5d68ea70c42bb7b400891eda7ea178cbfec3
victorcs <victo.santiago@grupopan.com>
2020-03-06 15:22:43 -0300

    chore: Added mappers and refactor entities and dtos.

---
+++ app/src/main/java/br/com/android/victorcs/mvvmarvel/data/mappers/CharacterMapper.kt
@@ -0,0 +1,52 @@
+package br.com.android.victorcs.mvvmarvel.data.mappers
+
+import br.com.android.victorcs.mvvmarvel.data.model.*
+import br.com.android.victorcs.mvvmarvel.domain.model.*
+
+fun Character.map(): CharacterDto = CharacterDto(
+    comics = comics?.map(),
+    description = description,
+    events = events?.map(),
+    id = id,
+    modified = modified,
+    name = name,
+    resourceURI = resourceURI,
+    series = series?.map(),
+    stories = stories?.map(),
+    thumbnail = thumbnail?.map(),
+    urls = urls?.mapUrlDto()
+)
+
+fun Comics.map(): ComicsDto = ComicsDto(
+    available = available,
+    collectionURI = collectionURI,
+    items = item?.mapGenericItemDto(),
+    returned = returned
+)
+
+fun GenericList.map(): GenericListDto = GenericListDto(
+    available = available,
+    returned = returned,
+    items = item?.mapGenericItemDto(),
+    collectionURI = collectionURI
+)
+
+fun Thumbnail.map(): ThumbnailDto = ThumbnailDto(
+    extension = extension,
+    path = path
+)
+
+fun List<GenericItem>.mapGenericItemDto(): List<GenericItemDto> = this.map { generic ->
+    GenericItemDto(
+        type = generic.type,
+        name = generic.name,
+        resourceUri = generic.resourceUri
+    )
+}
+
+fun List<Url>.mapUrlDto(): List<UrlDto> = this.map { mUrl ->
+    UrlDto(
+        type = mUrl.type,
+        url = mUrl.url
+    )
+}
